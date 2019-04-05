d0db467f88f995f2b1b95362c693d3fffb5a2482
harryxiang <harryxiang@tencent.com>
2019-04-05 20:57:33 +0800

    add CompileUtil and Compile

--- Compile.js
+++ Compile.js
@@ -19,23 +19,66 @@ class Compile{
     }
 
     compile(fragment){
+        //将文档碎片转化为扁平的节点数组
+        let childNodes = fragment.childNodes;
+
+        //Array.from的作用是将类数组转换为数组， 这里是遍历解析数组内的节点
+        Array.from(childNodes).forEach((node)=>{
+            if( this.isElementNode(node)){
+                //当节点为元素节点时
+
+                //1. 递归编译子元素
+                this.compile(node);
+
+                //2. 编译当前节点
+                this.compileElement(node);
+            }else{
+                //当节点为文本节点，直接编译
+                this.compileText(node);
+            }
+        })
+
 
     }
 
-    compileElement(){
+    //元素节点编译
+    compileElement(node){
+        let attrs = node.attributes;
+        Array.from(attrs).forEach(attr => {
+            let attrName = attr.name;
 
+            if(this.isDireactive(attrName)){
+                //如果是指令， 解析出 指令方法和表达式
+                let [,type] = attrName.split('-');
+                let exp = attr.value;
+
+                //todo:调用指令处理方法
+            }
+        })
     }
 
-    compileText(){
+    //文本节点编译
+    compileText(node){;
+        //匹配{{}}
+        //文本节点的内容有可能存在 “{{ }} {{ }} {{ }}”，正则匹配默认是贪婪的，为了防止第一个 “{” 和最后一个 “}” 进行匹配，所以在正则表达式中应使用非贪婪匹配。
+        let reg = /\{\{([^}+])\}\}/g;
+        let _text = node.textContent;
 
+        if(reg.test(_text)){
+            //todo: 处理绑定变量 
+        }
     }
-
 
 
     /* 辅助方法  */
     //判断是否元素节点
     isElementNode(el){
         return el.nodeType === 1  // 1-元素节点   3-文本节点
+    }
+
+    //判断属性是否为指令
+    isDireactive(attrName){
+        return attrName.includes("v-");
     }
 
     /* 核心方法 */
